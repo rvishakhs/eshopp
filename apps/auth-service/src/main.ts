05d377bf96e364a1043d4cc1b0acfc7294998b8a
rvishakhs <rvishakhs@gmail.com>
2025-09-15 13:10:55 +0100

    Making essential security measures to the api gateway

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+import proxy from 'express-http-proxy';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import swaggerUi from 'swagger-ui-express';
+import axios from 'axios';
+import cookieParser from 'cookie-parser';
+
+
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 6000;
 
 const app = express();
+
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
